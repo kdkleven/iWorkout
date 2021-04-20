ecd3838a30fd506953e5f7d6efd3a650b362c93c
Kyle Kleven <kylekleven8@gmail.com>
2021-04-19 23:29:52 -0500

    deploying

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,54 @@
 const router = require('express').Router();
+const path = require("path");
+const {Exercise} = require("../models/exercises.js");
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+});
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.get("/stats", (req, res) => {
+    
+    res.sendFile(path.join(__dirname, "../public", 'stats.html'));
+
+});
+
+//GET /api/workouts to get the last workout
+router.get("/exercise", (req, res) => {
+    Exercise.find({})
+    .then(dbExercise => {
+        res.json(dbExercise);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+//PUT /api/workouts to add exercise
+router.put("/exercise", (req, res) => {
+    
+});
+
+//POST /api/workouts to create workout
+router.post("/exercise", (req, res) => {
+    body = req.body;
+    Exercise.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+//GET /api/workouts/range to get workouts in range
+router.get("/exercise", (req, res) => {
+    Exercise.find({})
+    .then(dbExercise => {
+        res.json(dbExercise);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
+
 const PORT = process.env.PORT || 3000;
 
-const db = require("/models");
+const db = require("./models/exercises");
 
 db.on("error", error => {
   console.log("Database Error:", error);
@@ -19,13 +20,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 // routes
-app.use(require("./controllers/dashboardRoutes.js"));
+app.use(require("./controllers/"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
